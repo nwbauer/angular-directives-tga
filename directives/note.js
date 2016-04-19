d4cb049ba418077fed835775a993a9e330b53265
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2016-04-19 09:50:33 -0700

    added comments

--- directives/note.js
+++ directives/note.js
@@ -2,20 +2,32 @@ angular.module('notebook.directives')
 
 .directive('myNote', function(){
 
+  //returns an object that defines the directive (how it looks and acts)
   return {
+
+    //how should I look?
     template: '<div>Note {{::index}} {{::note.message}}</div>',
+
+    //isolated scope so that I create my own universe
+    //what configuration parameters do I need to be passed in?
     scope:{
       note:'=',
       index:'=',
     },
+
+    //how should I update my UI based on my state ($scope values)?
     link: function(scope, elem, attrs) {
 
+      //register a behavior
       elem.on('click', function(event){
 
+        //based on my state, update the interface
         if(scope.index === 1) {
           elem.css({color: 'rgb(225,0,0)'});
         }
 
+        //tell the parent controllers that I was clicked
+        //teamwork!
         scope.$emit('note:clicked');
       });
     },
--- directives/noteList.js
+++ directives/noteList.js
@@ -2,18 +2,40 @@ angular.module('notebook.directives', ['NoteService'])
 
 .directive('noteList', function(){
 
+  //returns an object that defines the directive (how it looks and acts)
   return {
+
+    //how should I look?
     template: '<div>Times Clicked: {{numberClicked}}</div><my-note index="$index" note="::singleNote" ng-repeat="singleNote in notes"></my-note>',
-    scope:{
-      version:'=',
-    },
+
+    //isolated scope so that I create my own universe
+    //what configuration parameters do I need to be passed in?
+    scope:{},
+
+    //how should I intract with other services/directives?
     controller: function($scope, NoteFactory) {
+
+      //=============================
+      //initalize our state
       $scope.numberClicked = 0;
+
+      //access an external data source (NoteFactory)
       $scope.notes = NoteFactory.getNotes(1000);
 
+
+      //=============================
+      //react to events that other directives emit
+      //teamwork!
       $scope.$on('note:clicked', function(){
+
+        //update our own state
         $scope.numberClicked++;
+
+        //this is needed because this event happens out of the regular digest cycle
+        //so without this, the state will update, but the UI will not
         $scope.$apply();
+
+        //just verify that the event was actually heard by this directive
         console.log('noteList heard clicked');
       });
     },
